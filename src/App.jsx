dc6b73231085991db98d0f8e7badaae22aa2e85b
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-19 12:36:16 +0300

    create Layout route and add other routes as its children

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import Menu from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import Order from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
 
 const router = createBrowserRouter(
-    [{path: '/', element: <Home/>},
-        {path: '/menu', element: <Menu/>},
-        {path: '/cart', element: <Cart/>},
-        {path: '/order/new', element: <CreateOrder/>},
-        {path: '/order/:orderId', element: <Order/>}])
+    [{
+        element: <AppLayout/>, children:
+            [{path: '/', element: <Home/>},
+                {path: 'menu', element: <Menu/>},
+                {path: 'cart', element: <Cart/>},
+                {path: 'order/new', element: <CreateOrder/>},
+                {path: 'order/:orderId', element: <Order/>}]
+    },
+    ])
 export default function App() {
     return <RouterProvider router={router}/>
 }
