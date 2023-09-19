467fb3a9af7fdd346e60e11043443b525918b47d
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-19 12:54:55 +0300

    render the page after fetching the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 import Home from "./ui/Home.jsx";
-import Menu from "./features/menu/Menu.jsx";
+import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
 import Order from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
@@ -11,7 +11,7 @@ const router = createBrowserRouter(
     [{
         element: <AppLayout/>, children:
             [{path: '/', element: <Home/>},
-                {path: 'menu', element: <Menu/>},
+                {path: 'menu', element: <Menu/>, loader: menuLoader},
                 {path: 'cart', element: <Cart/>},
                 {path: 'order/new', element: <CreateOrder/>},
                 {path: 'order/:orderId', element: <Order/>}]
