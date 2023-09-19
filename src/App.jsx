733f205e9c2b0d4cfd18f08e24cff627ab0a5e7d
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-19 12:17:23 +0300

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
-export default function App() {
-    return <div>
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import Order from "./features/order/Order.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
 
-    </div>
+const router = createBrowserRouter(
+    [{path: '/', element: <Home/>},
+        {path: '/menu', element: <Menu/>},
+        {path: '/cart', element: <Cart/>},
+        {path: '/order/new', element: <CreateOrder/>},
+        {path: '/order/:orderId', element: <Order/>}])
+export default function App() {
+    return <RouterProvider router={router}/>
 }
