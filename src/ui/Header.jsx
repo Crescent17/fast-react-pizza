0decc29455161d8a9ffd8bc6702c9d937ff57de0
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-19 15:27:02 +0300

    add SearchOrder component into Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,10 @@
 import {Link} from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder.jsx";
 
 export default function Header() {
     return <header>
         <Link to='/'>Fast React Pizza Co.</Link>
+        <SearchOrder/>
         <p>Marat</p>
     </header>
 }
