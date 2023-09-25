4f2d4051b14f4d9683b37bf06e3cd8ce804916b5
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-25 10:49:06 +0300

    style header component using tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,11 @@
 import {Link} from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/user/Username.jsx";
 
 export default function Header() {
-    return <header>
-        <Link to='/'>Fast React Pizza Co.</Link>
+    return <header className='bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6'>
+        <Link to='/' className='tracking-widest'>Fast React Pizza Co.</Link>
         <SearchOrder/>
-        <p>Marat</p>
+        <Username/>
     </header>
 }
