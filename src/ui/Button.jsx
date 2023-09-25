a9e5feba0415ad5bed230bc3824f647fd1c0bd94
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-25 17:59:24 +0300

    create 2 button types

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,18 @@
 import {Link} from "react-router-dom";
 
-export default function Button({children, disabled, to}) {
-    const className = `bg-yellow-400 transition-colors uppercase font-semibold text-stone-800
-    py-3 px-4 inline-block tracking-wide rounded-full outline-none hover:bg-yellow-300
+export default function Button({children, type, disabled, to}) {
+    const base = `bg-yellow-400 transition-colors uppercase font-semibold text-stone-800
+    inline-block tracking-wide rounded-full outline-none hover:bg-yellow-300
     duration-300 focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2
-    disabled:cursor-not-allowed sm:px-6 sm:py-4`
-    if (to) {
-        return <Link to={to} className={className}>{children}</Link>
+    disabled:cursor-not-allowed `
+
+    const styles = {
+        primary: base + 'py-3 px-4 md:px-6 md:py-4',
+        small: base + 'py-2 px-4 sm:px-5 sm:py-2.5 text-xs'
     }
-    return <button disabled={disabled} className='bg-yellow-400 transition-colors uppercase font-semibold text-stone-800
-                            py-3 px-4 inline-block tracking-wide rounded-full outline-none hover:bg-yellow-300
-                            duration-300 focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2
-                            disabled:cursor-not-allowed sm:px-6 sm:py-4'>{children}</button>
+
+    if (to) {
+        return <Link to={to} className={styles[type]}>{children}</Link>
+    }
+    return <button disabled={disabled} className={styles[type]}>{children}</button>
 }
