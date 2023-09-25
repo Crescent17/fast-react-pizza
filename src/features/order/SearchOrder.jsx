f202ec531af5970004c439d0714fba51a606173d
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-25 14:43:57 +0300

    apply styles for search input

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -13,6 +13,7 @@ export default function SearchOrder() {
     }
 
     return <form onSubmit={handleSubmit}>
-        <input placeholder='Search order #' value={query} onChange={e => setQuery(e.target.value)}/>
+        <input placeholder='Search order #' value={query} className='rounded-full px-4 py-2 text-sm
+        bg-yellow-100 placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-yellow-500' onChange={e => setQuery(e.target.value)}/>
     </form>
 }
