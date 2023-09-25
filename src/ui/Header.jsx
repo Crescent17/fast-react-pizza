dbb5b82d0016e51c53d0d68d87c4eb3909d072a0
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-25 11:05:42 +0300

    position header items using flexbox

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder.jsx";
 import Username from "../features/user/Username.jsx";
 
 export default function Header() {
-    return <header className='bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6'>
+    return <header className='flex items-center justify-between bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6'>
         <Link to='/' className='tracking-widest'>Fast React Pizza Co.</Link>
         <SearchOrder/>
         <Username/>
