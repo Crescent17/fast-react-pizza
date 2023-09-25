bde8bedb2075fcff14a89ec27641205586e0f644
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-25 18:22:11 +0300

    add secondary button type

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,7 +8,11 @@ export default function Button({children, type, disabled, to}) {
 
     const styles = {
         primary: base + 'py-3 px-4 md:px-6 md:py-4',
-        small: base + 'py-2 px-4 sm:px-5 sm:py-2.5 text-xs'
+        small: base + 'py-2 px-4 sm:px-5 sm:py-2.5 text-xs',
+        secondary: `border-2 border-stone-300 transition-colors uppercase font-semibold text-stone-400
+    inline-block tracking-wide rounded-full outline-none hover:bg-stone-300 hover:text-stone-800
+    duration-300 focus:ring focus:ring-stone-200 focus:text-stone-800 focus:bg-stone-300 focus:ring-offset-2
+    disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5`
     }
 
     if (to) {
