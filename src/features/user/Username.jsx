9041ec152253559b33435613308adba2d1c80ae7
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-26 13:57:47 +0300

    display username only in case it exists

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,9 @@
+import {useSelector} from "react-redux";
+
 export default function Username() {
+    const username = useSelector(state => state.user.username)
+    if (!username) return null
     return <p className='text-sm font-semibold hidden md:block'>
-        Marat
+        {username}
     </p>
 }
