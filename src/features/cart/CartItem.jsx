d50cd00482ff09807d7c6d7758aa46c0e232180a
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-26 21:01:26 +0300

    use DeleteItem component to delete pizza from cart

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,20 +1,20 @@
 import {formatCurrency} from "../../utils/helpers.js";
-import Button from "../../ui/Button.jsx";
+import DeleteItem from "./DeleteItem.jsx";
 
-function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+function CartItem({item}) {
+    const {pizzaId, name, quantity, totalPrice} = item;
 
-  return (
-    <li className='py-3 sm:flex sm:items-center sm:justify-between'>
-      <p className='mb-1 sm:mb-0'>
-        {quantity}&times; {name}
-      </p>
-      <div className='flex justify-between items-center sm:gap-6'>
-        <p className='text-sm font-bold'>{formatCurrency(totalPrice)}</p>
-          <Button type='small'>Delete</Button>
-      </div>
-    </li>
-  );
+    return (
+        <li className='py-3 sm:flex sm:items-center sm:justify-between'>
+            <p className='mb-1 sm:mb-0'>
+                {quantity}&times; {name}
+            </p>
+            <div className='flex justify-between items-center sm:gap-6'>
+                <p className='text-sm font-bold'>{formatCurrency(totalPrice)}</p>
+                <DeleteItem pizzaId={pizzaId}>Delete</DeleteItem>
+            </div>
+        </li>
+    );
 }
 
 export default CartItem;
