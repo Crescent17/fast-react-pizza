7e3bf4b4aa36ca2b0293549b18dca29f25bbd7ef
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-26 19:24:13 +0300

    replace fake cart with cart from state, clear cart on button click, display EmptyCart component if the cart is empty

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,35 +1,17 @@
 import LinkButton from "../../ui/LinkButton.jsx";
 import Button from "../../ui/Button.jsx";
 import CartItem from "./CartItem.jsx";
-import {useSelector} from "react-redux";
-
-const fakeCart = [
-    {
-        pizzaId: 12,
-        name: 'Mediterranean',
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: 'Vegetable',
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: 'Spinach and Mushroom',
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
-];
+import {useDispatch, useSelector} from "react-redux";
+import {clearCart, getCart} from "./cartSlice.js";
+import {getUsername} from "../user/userSlice.js";
+import EmptyCart from "./EmptyCart.jsx";
 
 function Cart() {
-    const cart = fakeCart;
-    const username = useSelector(state => state.user.username)
+    const cart = useSelector(getCart);
+    const username = useSelector(getUsername);
+    const dispatch = useDispatch();
+
+    if (!cart.length) return <EmptyCart/>
 
     return (
         <div className='py-3 px-4'>
@@ -41,7 +23,7 @@ function Cart() {
             </ul>
             <div className='mt-6 space-x-2'>
                 <Button type='primary' to="/order/new">Order pizzas</Button>
-                <Button type='secondary'>Clear cart</Button>
+                <Button onClick={() => dispatch(clearCart())} type='secondary'>Clear cart</Button>
             </div>
         </div>
     );
