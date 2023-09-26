3d8aed3c439cf38a51fc7468969d6468f2ce3a7b
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-26 21:02:24 +0300

    get pizza quantity and if it's present in the cart display delete button instead of add to cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
 import {formatCurrency} from "../../utils/helpers.js";
 import Button from "../../ui/Button.jsx";
-import {useDispatch} from "react-redux";
-import {addItem} from "../cart/cartSlice.js";
+import {useDispatch, useSelector} from "react-redux";
+import {addItem, getCurrentQuantityById} from "../cart/cartSlice.js";
+import DeleteItem from "../cart/DeleteItem.jsx";
 
 function MenuItem({pizza}) {
     const dispatch = useDispatch()
     const {id, name, unitPrice, ingredients, soldOut, imageUrl} = pizza;
+    const currentQuantity = useSelector(getCurrentQuantityById(id))
+    const isInCart = currentQuantity > 0;
 
     function handleAddToCart() {
         const newItem = {pizzaId: id, name: name, quantity: 1, unitPrice: unitPrice, totalPrice: unitPrice * 1}
@@ -21,7 +24,8 @@ function MenuItem({pizza}) {
                 <div className='mt-auto flex items-center justify-between'>
                     {!soldOut ? <p className='text-sm'>{formatCurrency(unitPrice)}</p> :
                         <p className='text-sm uppercase font-medium text-stone-500'>Sold out</p>}
-                    {!soldOut && <Button type='small' onClick={handleAddToCart}>Add to cart</Button>}
+                    {isInCart && <DeleteItem pizzaId={id}>Delete</DeleteItem>}
+                    {!soldOut && !isInCart && <Button type='small' onClick={handleAddToCart}>Add to cart</Button>}
                 </div>
             </div>
         </li>
