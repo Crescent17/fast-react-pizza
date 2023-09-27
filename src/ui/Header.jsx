12725d70d758ee5818fde8ad5ba574c5fcf19096
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-27 20:05:01 +0300

    add conditions to header navigation

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,14 @@
 import {Link} from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
 import Username from "../features/user/Username.jsx";
+import {useSelector} from "react-redux";
+import {getUsername} from "../features/user/userSlice.js";
 
 export default function Header() {
-    return <header className='flex items-center justify-between bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6'>
-        <Link to='/menu' className='tracking-widest'>Fast React Pizza Co.</Link>
+    const username = useSelector(getUsername)
+    return <header
+        className='flex items-center justify-between bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6'>
+        <Link to={username ? '/menu' : '/'} className='tracking-widest'>Fast React Pizza Co.</Link>
         <SearchOrder/>
         <Username/>
     </header>
