8ead124d9497210385bb0f8e2a3d6cb9b71e73ea
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-27 16:04:53 +0300

    create new round button type

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -9,6 +9,7 @@ export default function Button({children, type, disabled, to, onClick}) {
     const styles = {
         primary: base + 'py-3 px-4 md:px-6 md:py-4',
         small: base + 'py-2 px-4 sm:px-5 sm:py-2.5 text-xs',
+        round: base + 'py-1 px-2.5 sm:px-3.5 sm:py-2 text-sm',
         secondary: `text-sm border-2 border-stone-300 transition-colors uppercase font-semibold text-stone-400
     inline-block tracking-wide rounded-full outline-none hover:bg-stone-300 hover:text-stone-800
     duration-300 focus:ring focus:ring-stone-200 focus:text-stone-800 focus:bg-stone-300 focus:ring-offset-2
