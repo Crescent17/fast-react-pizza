f382640990b7f9cade4cd37beb2ac63b6564c67c
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-28 12:34:07 +0300

    add an action to specific order route

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 import Home from "./ui/Home.jsx";
 import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
-import Cart from "./features/cart/Cart.jsx";
 import Order, {loader as orderLoader} from "./features/order/Order.jsx";
+import {action as updateOrderAction} from './features/order/UpdateOrder.jsx'
+import Cart from "./features/cart/Cart.jsx";
 import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
@@ -15,7 +16,13 @@ const router = createBrowserRouter(
                 {path: 'menu', element: <Menu/>, loader: menuLoader, errorElement: <Error/>},
                 {path: 'cart', element: <Cart/>},
                 {path: 'order/new', element: <CreateOrder/>, action: createOrderAction},
-                {path: 'order/:orderId', element: <Order/>, loader: orderLoader, errorElement: <Error/>,}]
+                {
+                    path: 'order/:orderId',
+                    element: <Order/>,
+                    loader: orderLoader,
+                    errorElement: <Error/>,
+                    action: updateOrderAction
+                }]
     },
     ])
 export default function App() {
